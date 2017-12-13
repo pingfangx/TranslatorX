0c4508642c6c7f009ea14862a5d04dff6854521f
pingfangx <pingfangx@pingfangx.com>
2017-12-13 16:19:16 +0800

    删除 Android Studio 及相关分支，已合并到 JetBrains 中

--- AndroidStudio/original/pseudo.bat
+++
@@ -1,5 +0,0 @@
-set omegat_jar=D:\xx\software\program\OmegaT_4.1.2_01_Beta_Without_JRE\OmegaT.jar
-set project_dir=D:\workspace\TranslatorX\AndroidStudio
-set filename=pseudo.tmx
-java -jar %omegat_jar% %project_dir% --mode=console-createpseudotranslatetmx --pseudotranslatetmx=%filename% --pseudotranslatetype=empty
-pause
